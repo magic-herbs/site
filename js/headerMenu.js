0d1622066c119a5529ea4a836352fa6ef25d5436
magic-herb <ivan.prueba.python@gmail.com>
2023-04-02 15:55:29 -0500

    Actualizacion en el menu del header

---
+++ js/headerMenu.js
@@ -0,0 +1,30 @@
+const menuBtn = document.getElementById('menuBtn');
+
+
+menuBtn.addEventListener('click', ()=>{
+    document.getElementById('menuList').classList.toggle('menu-hidden');
+    // document.getElementById('headerMenuList').classList.toggle('hidden');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
