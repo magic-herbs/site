ee5a97af684784927194987dca9c63d2c54db045
magic-herb <ivan.prueba.python@gmail.com>
2023-04-04 09:28:48 -0500

    Nuevos scripts
    Separados los productos de los viajes y servicios
    Añadido script para listar los productos en su respectiva página

--- js/productList.js
+++ js/productList.js
@@ -1,90 +1,49 @@
-travel(
-    [
-        {
-            "product": "Vieaje al Sol",
-            "price": "$229,999,999,999,998.00 USD",
-            "description": {
-                "short": "Este viaje épico es una oportunidad única en la vida para explorar la estrella más cercana a la Tierra. Pero no te preocupes, nuestro equipo de expertos ha diseñado el viaje de manera segura, ¡se viaja de noche para evitar quemaduras!",
-                "extended": "¿Estás listo para experimentar lo último en viajes espaciales? ¡Te presentamos nuestro viaje al sol!\nEste viaje épico es una oportunidad única en la vida para explorar la estrella más cercana a la Tierra. Pero no te preocupes, nuestro equipo de expertos ha diseñado el viaje de manera segura, ¡se viaja de noche para evitar quemaduras!\n¡Recuerda que se tiene que viajar de noche para evitar quemaduras! Pero no te preocupes, nuestros expertos han diseñado el viaje de manera que puedas disfrutar del sol sin preocupaciones\nNo nos hacemos responsables de ninguna enfermedad ocasionada por los rayos solares. Aunque hemos tomado medidas de seguridad para garantizar tu bienestar, es importante que tomes precauciones adicionales y te protejas del sol.\nEl precio anunciado es solo ida. Sin embargo, ofrecemos un precio justo para el viaje de vuelta al mismo precio que el viaje de ida.\nExperimenta la sensación única de estar más cerca del sol que cualquier otra persona en la historia. ¡Reserva tu viaje al sol hoy mismo y descubre un nuevo mundo de aventura y exploración espacial!"
-            },
-            "image": {
-                "src": "./img/spacecraft_traveling_around_the_sun.jpg",
-                "alt": "Imagen del sol generada por IA"
-            }
-        },
-        {
-            "product": " 3 vueltas a la tierra ",
-            "price": "$4.000.000",
-            "description": {
-                "short": "Este viaje único en la vida te llevará más allá de los límites de la Tierra y te llevará a través de la atmósfera hacia el espacio. Experimentarás la ingravidez, verás la Tierra desde una perspectiva completamente nueva y experimentarás la aventura del espacio como nunca antes.",
-                "extended": "Si estás buscando una experiencia única y emocionante, ¡no busques más allá de nuestro viaje espacial!\nEste viaje único en la vida te llevará más allá de los límites de la Tierra y te llevará a través de la atmósfera hacia el espacio. Experimentarás la ingravidez, verás la Tierra desde una perspectiva completamente nueva y experimentarás la aventura del espacio como nunca antes.\nComenzaremos nuestro viaje en la base de lanzamiento en algun lugar de la selva amazonica. Allí, te prepararemos para tu viaje espacial con un equipo de expertos en astronautas que te guiarán a través de todo el proceso. Una vez que estés listo, subirás a bordo de una nave espacial de última generación y comenzarás tu emocionante viaje hacia el espacio.\nAl alcanzar la órbita terrestre, tendrás la oportunidad de experimentar la ingravidez mientras flotas libremente en el espacio. También tendrás la oportunidad de ver la Tierra desde una perspectiva completamente nueva mientras orbitamos a su alrededor. Verás el planeta azul desde arriba, con una vista panorámica que solo unos pocos afortunados han experimentado.\nEste viaje espacial es la aventura más emocionante que puedas imaginar y será una experiencia que nunca olvidarás. Así que, si estás listo para viajar al espacio y vivir la aventura de tu vida, ¡no esperes más para reservar tu lugar en nuestro emocionante viaje espacial! ¡Te esperamos!"
-            },
-            "image": {
-                "src": "./img/earth.jpg",
-                "alt": "Imagen de la tierra generada por IA"
-            }
-        },
-        {
-            "product": "Viaje a la Luna",
-            "price": "$500,000,000.00 USD",
-            "description": {
-                "short": "¿Estás cansado de las mismas viejas vacaciones en la playa o en las montañas? ¡Es hora de llevar tus vacaciones al siguiente nivel y visitar la luna!",
-                "extended": "¿Estás cansado de las mismas viejas vacaciones en la playa o en las montañas? ¡Es hora de llevar tus vacaciones al siguiente nivel y visitar la luna!\n¿No es eso lo más loco que has oído en tu vida? Imagina la cantidad de likes que tendrás en Instagram.\ncomo en todo, hay algunos contras. Por ejemplo, la gravedad en la luna es un poco diferente que la de la Tierra, así que tendrás que acostumbrarte a saltar en slow-motion. Y si te sientes un poco mal del estómago, recuerda que los baños en la luna son un poco extraños ya que no hay gravedad. Asegúrate de tener a la mano una toalla o dos.\nEn resumen, si estás buscando unas vacaciones fuera de lo común, entonces definitivamente deberías reservar tu lugar en nuestro viaje a la luna. Podrás disfrutar de la vista más espectacular que hayas visto en tu vida, experimentar la sensación única de gravedad lunar y tal vez, sólo tal vez, encontrar vida en otro planeta. ¡No lo dudes más y reserva hoy mismo tu viaje lunar!"
-            },
-            "image": {
-                "src": "./img/moon.jpg",
-                "alt": "Imagen de la tierra generada por IA"
-            }
-        },
-        {
-            "product": "Viaje a Saturno",
-            "price": "$1,200,000,000.00 USD",
-            "description": {
-                "short": "Sabemos que la mayoría de la gente se emociona con los anillos de Saturno, pero ¿sabías que sus lunas son igual de impresionantes? Si quieres ver con tus propios ojos cada una de las lunas de Saturno, te ofrecemos un paquete exclusivo que incluye un paseo turístico en cada una de las lunas del planeta, por un costro extra claro.",
-                "extended": "¿Estás preparado para aventurarte en uno de los planetas más fascinantes del sistema solar? ¡Te presentamos nuestro viaje a Saturno!\nSabemos que la mayoría de la gente se emociona con los anillos de Saturno, pero ¿sabías que sus lunas son igual de impresionantes? Si quieres ver con tus propios ojos cada una de las lunas de Saturno, te ofrecemos un paquete exclusivo que incluye un paseo turístico en cada una de las lunas del planeta, por un costro extra claro.\nEso sí, debemos advertirte que estas lunas son como nada que hayas visto antes. Por ejemplo, en la luna Titán, la única luna con una atmósfera densa, tendrás que llevar un tanque de oxígeno y traje térmico, ya que hace más frío que en casa de mi exsuegra. Y la luna Encélado, aunque tiene una superficie congelada, esconde debajo de ella un océano subterráneo caliente...¡por lo que tendrás que tener cuidado de no caerte en un agujero de gusano o en una piscina de agua caliente!\nAdemás, si contratas nuestro paquete extra, tendrás la oportunidad de participar en una cena de gala en el palacio de hielo de Saturno, donde tendrás la oportunidad de conocer a sus habitantes más peculiares, incluyendo a un par de hombres de hielo y un erizo espacial.\nAsí que, ¿estás preparado para una experiencia única en la vida en nuestro viaje a Saturno y sus lunas? ¡Reserva ya tu lugar y descubre todo lo que el universo tiene para ofrecer!"
-            },
-            "image": {
-                "src": "./img/saturn.jpg",
-                "alt": "Imagen de Saturno generada por IA"
-            }
-        },
-        {
-            "product": "Viaje a Urano",
-            "price": "2,500,000,000.00 USD",
-            "description": {
-                "short": "Urano es un planeta muy particular. Por un lado, su atmósfera está llena de gas metano, lo que le da su característico color azul verdoso. Y por otro lado, Urano es uno de los planetas más fríos del sistema solar, ¡así que asegúrate de empacar tus abrigos de invierno y botas de nieve!",
-                "extended": "¿Estás listo para explorar uno de los planetas más misteriosos del sistema solar? ¡Te presento nuestro viaje a Urano!\nUrano es un planeta muy particular. Por un lado, su atmósfera está llena de gas metano, lo que le da su característico color azul verdoso. Y por otro lado, Urano es uno de los planetas más fríos del sistema solar, ¡así que asegúrate de empacar tus abrigos de invierno y botas de nieve!\nPero no te preocupes, en nuestro viaje a Urano, no solo podrás disfrutar de los paisajes helados y los fuertes vientos de más de 900 km/h, sino que también podrás ver algunas de las características más extrañas de Urano, como sus anillos oscuros y su inclinación axial única... Lo que sea que eso signifique."
-            },
-            "image": {
-                "src": "./img/uranus.jpg",
-                "alt": "Imagen de Urano generada por IA"
-            }
-        },
-        {
-            "product": "Viaje a Pluton",
-            "price": "$500,000,000,000.00 USD",
-            "description": {
-                "short": "Como sabes, ningún viaje está exento de riesgos, y Plutón no es la excepción. Debemos advertirte que las temperaturas en Plutón pueden bajar hasta los -240 grados Celsius, así que asegúrate de empacar suficientes mantas y abrigos para mantenerte caliente.",
-                "extended": "¡Bienvenidos al viaje familiar más emocionante del sistema solar! Hoy, te ofrezco la oportunidad de llevar a tu familia a Plutón, el planeta enano más lejano y misterioso.\nComo sabes, ningún viaje está exento de riesgos, y Plutón no es la excepción. Por un lado, debemos advertirte que las temperaturas en Plutón pueden bajar hasta los -240 grados Celsius, así que asegúrate de empacar suficientes mantas y abrigos para mantenerte caliente. Además, hay una pequeña posibilidad de que te encuentres con algunos extraterrestres hostiles durante el viaje. Pero no te preocupes, ¡nuestro equipo de seguridad siempre está listo para luchar contra cualquier invasión alienígena!\nEn este viaje, podrás explorar las regiones más frías y remotas del sistema solar y descubrir los secretos ocultos de este pequeño planeta."
-            },
-            "image": {
-                "src": "./img/pluto.jpg",
-                "alt": "Imagen de pluton generada por IA"
-            }
-        }
-    ]
-);
-
 products(
     [
         {
             "product": "Corazón",
-            "price": "None",
-            "description": "",
+            "price": "por definir",
+            "description": "¡Si por algún motivo necesitas un trasplante de corazón y no tienes un hospital cerca aquí tenemos tu solución</br> Un corazón fresquito!!",
             "image": {
-                "src": "./img/pluto.jpg",
-                "alt": "Imagen de pluton generada por IA"
+                "src": "./img/contenedor.jpg",
+                "alt": "Imagen de un corazón"
             }
-        }
+        },
+        {
+            "product": "Hígado",
+            "price": "por definir",
+            "description": "Tu hígado no funciona correctamente?</br>Sacrifica a una persona desafortunada por tu bien!</br>Aquí lo hacemos por ti a bajo costo",
+            "image": {
+                "src": "./img/contenedor.jpg",
+                "alt": "Imagen de un higado"
+            }
+        },
+        {
+            "product": "Pulmones",
+            "price": "por definir",
+            "description": "El cigarrillo te ha estado pasando factura ¿verdad?</br>Aquí puedes conseguir un par de pulmones nuevos a un excelente costo!</br>Porque no hay nada más gratificante que quitarle algo alguien sabiendo que lo vas a seguir usando mal.",
+            "image": {
+                "src": "./img/contenedor.jpg",
+                "alt": "Imagen de un par de pulmones"
+            }
+        },
+        {
+            "product": "Cerebro",
+            "price": "por definir",
+            "description": "No sabemos para qué lo necesitas... y tampoco nos importa</br>Igual a nosotros también nos gusta quitarle cosas a los que si las necesitan aunque nosotros no.",
+            "image": {
+                "src": "./img/contenedor.jpg",
+                "alt": "Imagen de cerebro"
+            }
+        },
+        {
+            "product": "Riñones",
+            "price": "por definir",
+            "description": "A mí también me gusta mear tranquilo, por eso aquí te tenemos esta hermosa solución</br>¡Un par seminuevo de Riñones!...<br>pero espera!, sabiendo que tienes tantas ganas de adquirir el nuevo iPhone te queremos decir que... ¡También compramos riñones!",
+            "image": {
+                "src": "./img/contenedor.jpg",
+                "alt": "Imagen de riñones"
+            }
+        },
     ]
 );
--- js/products.js
+++ js/products.js
@@ -1,1 +1,23 @@
 // Por hacer
+
+const cardsContainer = document.getElementById("cardsContainer");
+
+let app = {};
+
+let products = (data) => {
+    html = "";
+    for (let i = 0; i < data.length; i++) {
+        html += `<div class="card">
+        <div class="image-container">
+            <img src="${data[i]['image']['src']}" alt="${data[i]['image']['alt']}">
+        </div>
+        <div class="text-container">
+            <h2 class="title">${data[i]['product']}</h2>
+            <h3 class="price">${data[i]['price']}</h3>
+            <p class="desc">${data[i]['description']}</p>
+        </div>
+    </div>
+    `;
+    }
+    cardsContainer.innerHTML += html;
+};
